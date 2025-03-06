b2b69a4fc0b0cabbbb396ef6f0c061abaa8fcfda
chan <white1614@inzent.com>
2025-03-06 17:12:47 +0900

    user test

--- src/main/java/study/spring/chan/controller/UserController.java
+++ src/main/java/study/spring/chan/controller/UserController.java
@@ -1,14 +1,20 @@
 package study.spring.chan.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import study.spring.chan.domain.UserDTO;
+import study.spring.chan.service.UserService;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+
+  private final UserService userService;
 
   @GetMapping("/user")
   public String userHome(Model model) {
@@ -16,5 +22,11 @@ public class UserController {
     UserDTO user = new UserDTO("찬영", 13);
     model.addAttribute("user", user);
     return "/user/home";
+  }
+
+  @GetMapping("/user/{userId}")
+  public String findUser(@PathVariable String userId) {
+
+    return "/user/findUser";
   }
 }
--- src/main/java/study/spring/chan/service/UserService.java
+++ src/main/java/study/spring/chan/service/UserService.java
@@ -1,7 +1,38 @@
 package study.spring.chan.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import study.spring.chan.domain.UserDTO;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @Service
 public class UserService {
+
+  Map<String, UserDTO> repository = new HashMap<>();
+
+  // 유저 만들기
+  public UserDTO createUser(String username, int age) {
+    UserDTO user = new UserDTO(username, age);
+
+    if(repository.containsKey(username)) {
+      throw new IllegalArgumentException(username + "이 존재합니다.");
+    }
+
+    repository.put(username, user);
+    return user;
+  }
+
+  // 유저 찾기
+  public UserDTO findUser(String username) {
+    return repository.get(username);
+  }
+
+  public void deleteUser(String username) {
+    log.info("유저가 삭제되었습니다. - {}", username);
+    repository.remove(username);
+  }
+
 }
