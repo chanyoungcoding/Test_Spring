00e3beec03dae622ca8c5108f5decdae6a9b4fa7
chan <white1614@inzent.com>
2025-03-06 16:27:08 +0900

    thymeleaf 수정중

--- src/main/java/study/spring/chan/controller/UserController.java
+++ src/main/java/study/spring/chan/controller/UserController.java
@@ -1,7 +1,13 @@
 package study.spring.chan.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+
+  @GetMapping("/user")
+  public String userHome() {
+    return "/userHome";
+  }
 }
